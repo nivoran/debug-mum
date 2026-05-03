145bf4833ce6006c6fc2927ca74fb5342a2b990d
nivoran <60741520+nivoran@users.noreply.github.com>
2026-05-03 22:18:16 +0300

    Add files via upload

--- AIDEV Cours MVC/Project/views/tasks.php
+++ AIDEV Cours MVC/Project/views/tasks.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mes tâches</title>
+</head>
+<body>
 
+<h2>Liste des tâches</h2>
+
+<form method="POST" action="index.php?action=add">
+    <input type="text" name="task" placeholder="Nouvelle tâche">
+    <button>Ajouter</button>
+</form>
+
+<ul>
+<?php foreach ($tasks as $task): ?>
+    <li><?= htmlspecialchars($task['title']) ?></li>
+<?php endforeach; ?>
+</ul>
+
+</body>
+</html>
