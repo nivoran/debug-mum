10fad926941eba4a9978d6bf71a3904ee3049cbd
nivoran <60741520+nivoran@users.noreply.github.com>
2026-05-03 22:15:25 +0300

    Add files via upload

--- AIDEV Cours MVC/Project/controllers/TaskController.php
+++ AIDEV Cours MVC/Project/controllers/TaskController.php
@@ -1,1 +1,33 @@
+<?php
 
+require_once "models/Task.php";
+
+class TaskController {
+
+    private $model;
+
+    public function __construct($pdo) {
+        $this->model = new Task($pdo);
+    }
+
+    public function handleRequest() {
+
+        $action = $_GET['action'] ?? 'list';
+
+        switch ($action) {
+
+            case 'add':
+                if (isset($_POST['task'])) {
+                    $this->model->addTask($_POST['task']);
+                }
+                header("Location: index.php?action=list");
+                break;
+
+            case 'list':
+            default:
+                $tasks = $this->model->getTasks();
+                require "views/tasks.php";
+                break;
+        }
+    }
+}
