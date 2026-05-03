d8c906c43115fb79185d9b39e7de3a741ff69aa3
nivoran <60741520+nivoran@users.noreply.github.com>
2026-05-03 22:16:52 +0300

    Add files via upload

--- AIDEV Cours MVC/Project/models/Task.php
+++ AIDEV Cours MVC/Project/models/Task.php
@@ -1,1 +1,20 @@
+<?php
 
+class Task {
+
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function addTask($task) {
+        $stmt = $this->pdo->prepare("INSERT INTO tasks (title) VALUES (?)");
+        $stmt->execute([$task]);
+    }
+
+    public function getTasks() {
+        $stmt = $this->pdo->query("SELECT * FROM tasks");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
